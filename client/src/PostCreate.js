717fd736f15a413edb76805f44e81df85aa4d8cc
take5150 <watanabetakehiro5150@gmail.com>
2024-03-23 08:20:01 +0900

    first commit

---
+++ client/src/PostCreate.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default () => {
+  return (
+    <div>
+      <form action="">
+        <div className="form-group">
+          <label htmlFor="">title</label>
+          <input type="text" className="form-control" />
+        </div>
+        <button className="btn btn-primary">Submit</button>
+      </form>
+    </div>
+  );
+};
